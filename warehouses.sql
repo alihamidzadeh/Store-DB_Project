ffbfdfdb325044dfe6e075286f1982e278e0a1e7
Tina Tavakkoli <tinatavakkoli81@gmail.com>
2023-01-06 07:20:00 +0330

    Added warehouses

---
+++ warehouses.sql
@@ -0,0 +1,44 @@
+INSERT INTO `storeproject`.`warehouse`
+(
+`city`,
+`country`,
+`address`,
+`postalCode`,
+`phoneNumber`)
+VALUES
+"mashhad",
+"iran",
+"hashemieh 67",
+"23458888889",
+"989053213280"
+);
+
+INSERT INTO `storeproject`.`warehouse`
+(
+`city`,
+`country`,
+`address`,
+`postalCode`,
+`phoneNumber`)
+VALUES
+"tehran",
+"iran",
+"zaferanieh",
+"1234567789",
+"989053213270"
+);
+
+INSERT INTO `storeproject`.`warehouse`
+(
+`city`,
+`country`,
+`address`,
+`postalCode`,
+`phoneNumber`)
+VALUES
+"shiraz",
+"iran",
+"hashemieh 67",
+"1357909743",
+"989053213260",
+);
