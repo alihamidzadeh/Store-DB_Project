07b5a74684830eccdce62550d6637cd85033fb73
tinatvk81 <122034471+tinatvk81@users.noreply.github.com>
2023-02-01 21:31:19 +0330

    Add files via upload

---
+++ supplier_sends_to_warehouse.sql
@@ -0,0 +1,34 @@
+INSERT INTO
+    `StoreProject`.`supplier_sends_to_warehouse` (
+        `Supplier_supplierID`,
+        `Warehouse_warehouseID`
+    )
+VALUES
+    (
+        "1",
+        "1"
+    );
+
+
+    INSERT INTO
+    `StoreProject`.`supplier_sends_to_warehouse` (
+        `Supplier_supplierID`,
+        `Warehouse_warehouseID`
+    )
+VALUES
+    (
+        "3",
+        "2"
+    );
+
+
+    INSERT INTO
+    `StoreProject`.`supplier_sends_to_warehouse` (
+        `Supplier_supplierID`,
+        `Warehouse_warehouseID`
+    )
+VALUES
+    (
+        "5",
+        "3"
+    );
---
+++ warehouse_has_item.sql
@@ -0,0 +1,64 @@
+INSERT INTO
+    `StoreProject`.`warehouse_has_item` (
+        `warehouseID`,
+        `itemID`,
+        `availableNum`,
+        `Supplier_supplierID`
+    )
+VALUES
+    (
+        "1",
+        "1",
+        "9",
+        "1"
+    );
+
+
+INSERT INTO
+    `StoreProject`.`warehouse_has_item` (
+    `warehouseID`,
+        `itemID`,
+        `availableNum`,
+        `Supplier_supplierID`
+    )
+VALUES
+    (
+
+        "2",
+        "2",
+        "11",
+        "3"
+    );
+
+
+    INSERT INTO
+    `StoreProject`.`warehouse_has_item` (
+        `warehouseID`,
+        `itemID`,
+        `availableNum`,
+        `Supplier_supplierID`
+    )
+VALUES
+    (
+        "3",
+        "3",
+        "7",
+        "5"
+    );
+
+
+
+    INSERT INTO
+    `StoreProject`.`warehouse_has_item` (
+        `warehouseID`,
+        `itemID`,
+        `availableNum`,
+        `Supplier_supplierID`
+    )
+VALUES
+    (
+        "1",
+        "4",
+        "4",
+        "2"
+    );
