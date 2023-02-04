16cedf585529ab84f1e656157df587a67f004853
tinatvk81 <122034471+tinatvk81@users.noreply.github.com>
2023-02-04 10:03:26 +0330

    Update pricehistory.sql

--- pricehistory.sql
+++ pricehistory.sql
@@ -7,10 +7,46 @@ INSERT INTO
 VALUES
     (
         "2021-02-09",
-        "1000000",
+        "600",
         "1"
     );
-
+    INSERT INTO
+    `StoreProject`.`pricehistory` (
+        `date`,
+        `price`,
+        `Item_itemID`
+    )
+VALUES
+    (
+        "2021-02-12",
+        "500",
+        "1"
+    );
+    
+  INSERT INTO
+    `StoreProject`.`pricehistory` (
+        `date`,
+        `price`,
+        `Item_itemID`
+    )
+VALUES
+    (
+        "2021-02-11",
+        "220000",
+        "2"
+    );
+INSERT INTO
+    `StoreProject`.`pricehistory` (
+        `date`,
+        `price`,
+        `Item_itemID`
+    )
+VALUES
+    (
+        "2021-02-16",
+        "500000",
+        "2"
+    );
 
     INSERT INTO
     `StoreProject`.`pricehistory` (
@@ -21,13 +57,10 @@ VALUES
 VALUES
     (
         "2021-03-19",
-        "1220000",
+        "12200",
         "3"
     );
-
-
-
-  INSERT INTO
+        INSERT INTO
     `StoreProject`.`pricehistory` (
         `date`,
         `price`,
@@ -35,7 +68,25 @@ VALUES
     )
 VALUES
     (
-        "2021-03-30",
-        "220000",
-        "2"
+        "2021-03-27",
+        "6000",
+        "3"
     );
+
+        INSERT INTO
+    `StoreProject`.`pricehistory` (
+        `date`,
+        `price`,
+        `Item_itemID`
+    )
+VALUES
+    (
+        "2021-03-21",
+        "400",
+        "4"
+    );
+
+    /*UPDATE pricehistory
+    SET price = "600"
+    WHERE  date= '2021-02-09 00:00:00';*/
+
